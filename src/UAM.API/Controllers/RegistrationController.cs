742e560b38dea6bd802810ba6f2d47c177759970
Nathan Healea <nate.healea@gmail.com>
2022-05-22 23:10:48 -0700

    Inital UAM.API with test controller

---
+++ src/UAM.API/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UAM.API.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/register")]
+    public class RegistrationController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterByEmailAndPassword()
+        {
+
+            return await Task.FromResult(Ok("pass"));
+        }
+    }
+}
---
+++ src/UAM.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(o =>
+{
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    o.ReportApiVersions = true;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
